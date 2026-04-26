ca654623f2986b62f5d153e79462c11e40fc53a9
Nilson Nascimento <nnascim@gmail.com>
2026-04-25 23:25:22 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const { Command, Option } = require('commander');
+
+const program = new Command();
+
+program
+  .command('create')
+  .description('Create a new workspace which includes a git worktree and a coding agent.')
+  .option('-n, --name <name>', 'workspace name')
+  .option('-s, --spec-file <specFile>', 'markdown spec file')
+  .addOption(new Option('-p, --prompt <prompt>', 'prompt to execute on start').conflicts('specFile'))
+  .option('-i, --init-script <initScript>', 'script to run after workspace creation')
+  .action((args) => {
+    console.log('Args:', args);
+  });
+
+
+program.parse(process.argv);
