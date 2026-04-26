fe8887086c6620d43d4048e57a7885f9b808cebb
Nilson Nascimento <nnascim@gmail.com>
2026-04-26 00:12:09 -0400

    Define command interface

--- index.cjs
+++ index.cjs
@@ -9,11 +9,12 @@ program
   .description('Create and manage AI coding agent workspaces')
   .command('create')
   .description('Create a new workspace which includes a git worktree and a coding agent.')
-  .option('-n, --name <name>', 'workspace name')
-  .option('-s, --spec-file <specFile>', 'markdown spec file')
-  .addOption(new Option('-p, --prompt <prompt>', 'prompt to execute on start').conflicts('specFile'))
-  .option('-i, --init-script <initScript>', 'script to run after workspace creation')
-  .action((args) => {
+  .argument('[name]', 'Workspace name. If omitted, the spec file name will be used instead.')
+  .option('-s, --spec-file <specFile>', 'Markdown spec file to launch the workspace with.')
+  .addOption(new Option('-p, --prompt <prompt>', 'Prompt to launch the workspace with. Can be used instead of specFile.').conflicts('specFile'))
+  .option('-i, --init-script <initScript>', 'Script to run after workspace creation. Can be used for tasks such as installing dependencies and running servers.')
+  .action((name, args) => {
+    console.log('Workspace name:', name);
     console.log('Args:', args);
   });
 
